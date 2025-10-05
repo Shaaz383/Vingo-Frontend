b8a38073a5c0a132ee6289db7a26155902a3435b
Shaaz383 <asadakhtar800@gmail.com>
2025-10-05 11:43:45 +0530

    Show homepage based on user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import UserDashboard from '../components/userDashboard'
+import OwnerDashboard from '../components/OwnerDashboard'
+import DeliveryBoy from '../components/DeliveryBoy'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
 
 const Home = () => {
+    const { userData } = useSelector((state) => state.user);
+    if(!userData){
+        return <Navigate to="/signin" />
+    }
+    if(userData.role === 'user'){
+        return <UserDashboard />
+    }
+    if(userData.role === 'owner'){
+        return <OwnerDashboard />
+    }
+    if(userData.role === 'deliveryBoy'){
+        return <DeliveryBoy />
+    }
   return (
     <div>Home</div>
   )
