1ec71a387c1a27ad44920fbcb66f07a402a65007
Shaaz383 <asadakhtar800@gmail.com>
2025-10-05 11:33:14 +0530

    Implement user authentication routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import useGetCurrentUser from "./hooks/useGetCurrentUser";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
-import useGetCurrentUser from "./hooks/useGetCurrentUser";
+import Home from "./pages/Home";
 
 function App() {
-  useGetCurrentUser()
-  return (
-  <Routes> 
-    <Route path="/signup" element={<SignUp />} />
-    <Route path="/signin" element={<SignIn />} />
-    <Route path="/forgot-password" element={<ForgotPassword/>} />
+  useGetCurrentUser();
+  const { userData } = useSelector((state) => state.user);
 
-    
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/signup" element={!userData ? <SignUp /> : <Home />} />
+      <Route path="/signin" element={!userData ? <SignIn /> : <Home />} />
+      <Route path="/forgot-password" element={!userData ? <ForgotPassword /> : <Home />} />
+      <Route path="/" element={userData ? <Home /> : <Navigate to="/signin" />} />
+    </Routes>
   );
 }
 
